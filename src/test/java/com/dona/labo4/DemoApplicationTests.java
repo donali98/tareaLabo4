67bfd622a43faafc5512c6ea346ff16d6f6e2098
Dona <00084417@uca.edu.sv>
2020-05-10 21:29:02 -0600

    Innitial commit

---
+++ src/test/java/com/dona/labo4/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dona.labo4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
